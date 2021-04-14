c4f0ace8ac7b9d9400d298b99359e10bb0a40c29
Jose Andres Hernandez <joseandreshernandezross@gmail.com>
2021-04-14 18:31:59 -0400

    Changes made

---
+++ app-assets/js/core/doctor/appointments.js
@@ -0,0 +1,236 @@
+import environment from '../../../../environment.js'
+import { checkLogin, logout, getCookie, checkLonginAdministrator } from './../general.js'
+
+$(document).ready(()=>{
+    getAppointments();
+})
+
+const appointmentFields = ["identifier", "name", "start", "end"]
+let clientMethod = '';
+
+
+function getAppointments(){
+
+    const request = $.ajax({
+        url: `${environment.apiURL}/v1/appointments`,
+        type: "get",
+        beforeSend: function(req) {
+            req.setRequestHeader("accept", "application/json");
+            req.setRequestHeader("Content-Type", "application/json");
+            req.setRequestHeader("Authorization", getCookie("Authorization"));
+          }
+    });
+    
+    request.done(function (response, textStatus, jqXHR){
+        let appointmentListHTML = ''
+        response.map(appointment => {
+            if(getCookie("identifier")!=appointment.identifier){
+                appointmentListHTML+=`<tr id='${appointment.identifier}'>
+                                    <th id='${appointment.identifier}-identifier'>${appointment.identifier}</th>
+                                    <th id='${appointment.identifier}-name'>${appointment.name}</th>
+                                    <th id='${appointment.identifier}-start'>${formatDate(appointment.start)}</th>
+                                    <th id='${appointment.identifier}-end'>${formatDate(appointment.end)}</th>
+                                    <th id='${appointment.identifier}-btn'>
+                                        <button onClick='window.editAppointment(`+JSON.stringify(appointment)+`)' type="button" class="btn btn-primary" data-toggle="modal" data-target="#appointmentsModal">
+                                            Edit
+                                        </button>
+                                        <button onClick='window.deleteAppointment(`+JSON.stringify(appointment)+`)' type="button" class="btn btn-danger">
+                                            Delete
+                                        </button>
+                                    </th>
+                                </tr>`
+            }
+        });
+
+        $("#appointments-list").html(appointmentListHTML)
+        
+    });
+    
+    request.fail(function (jqXHR, textStatus, errorThrown){
+        alert("Se ha producido un error en la consulta de usuarios")
+    });
+
+
+
+}
+
+window.editAppointment=(appointment)=>{
+
+    $("#form-error").text("")
+    appointmentFields.map(field => $(`#${field}-error`).hide() )
+
+   clientMethod="edit"
+   appointmentFields.map((field) => $(`#${field}`).val(String(appointment[field])) )
+
+   $("#start").val(formatDate($("#start").val()))
+   $("#end").val(formatDate($("#end").val()))
+    
+}
+
+function formatDate(date){
+
+    let d = new Date(date);
+    let ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d);
+    let mo = new Intl.DateTimeFormat('en', { month: '2-digit' }).format(d);
+    let da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(d);
+    return `${ye}-${mo}-${da}`
+}
+
+window.createAppointment=()=>{
+
+    $("#form-error").text("")
+    appointmentFields.map(field => $(`#${field}-error`).hide() )
+
+    clientMethod="create"
+    appointmentFields.map((field) => $(`#${field}`).val(""))
+
+}
+
+
+window.manageAppointment=()=>{
+
+    $("#form-error").text("")
+    appointmentFields.map(field =>{
+        $(`#${field}-error`).hide()
+        if(!$(`#${field}`).val()){
+            $(`#${field}-error`).show()
+        }
+    });
+
+    if(
+        !$("#name").val()       ||
+        !$("#start").val()      ||
+        !$("#end").val()      
+    ){
+        
+        return;
+
+    }
+
+    const identifier = $("#identifier").val();
+    const path = clientMethod=="create" ? "/v1/appointments" : `/v1/appointments/${identifier}`;
+
+    const appointmentData = {
+        
+            name: $("#name").val(), 
+            start: $("#start").val(), 
+            end: $("#end").val(),
+            repeat: true
+        
+    };
+
+    if(clientMethod=="create"){
+        appointmentData["user"]=getCookie("identifier");
+        appointmentData["app"]=getCookie("application");
+    }
+
+    $("#appointment-btn").hide()
+    $("#appointment-spinner").show()
+    
+    const request = $.ajax({
+        url: `${environment.apiURL}${path}`,
+        type: clientMethod=="create" ? "post" : "patch",
+        beforeSend: function(req) {
+            req.setRequestHeader("accept", "application/json");
+            req.setRequestHeader("Content-Type", "application/json");
+            req.setRequestHeader("Authorization", getCookie("Authorization"));
+            
+        },
+        data: JSON.stringify(appointmentData)
+    }); 
+
+
+    let actionWord=clientMethod=="create" ? "created" : "updated";
+     request.done((response)=>{
+
+        $("#appointment-btn").show()
+        $("#appointment-spinner").hide()
+        swal("Done", `The appointment was ${actionWord} succefully`, "success");
+        
+        if(clientMethod=="create"){
+            addAppointmentToList(actionWord=clientMethod=="create" ? response : appointmentData)
+        } else {
+            updateAppointmentInList({
+                identifier: $("#identifier").val(),
+                ...appointmentData
+            })
+        }
+    }); 
+
+    request.fail(function (jqXHR, textStatus, errorThrown){
+
+        $("#appointment-btn").show();
+        $("#appointment-spinner").hide();
+        $("#form-error").text(jqXHR.responseJSON.message)
+        swal("Done", `An error ocurred during request`, "warning");
+
+    });
+
+
+}
+
+
+function addAppointmentToList(appointment){
+    $("#appointments-list").append(
+        `<tr id='${appointment.identifier}'>
+                            <th id='${appointment.identifier}-identifier'>${appointment.identifier}</th>
+                            <th id='${appointment.identifier}-name'>${appointment.name}</th>
+                            <th id='${appointment.identifier}-start'>${formatDate(appointment.start)}</th>
+                            <th id='${appointment.identifier}-end'>${formatDate(appointment.end)}</th>
+                            <th id='${appointment.identifier}-btn'>
+                                <button onClick='window.editAppointment(`+JSON.stringify(appointment)+`)' type="button" class="btn btn-primary" data-toggle="modal" data-target="#appointmentsModal">
+                                    Edit
+                                </button>
+                                <button onClick='window.deleteAppointment(`+JSON.stringify(appointment)+`)' type="button" class="btn btn-danger">
+                                    Delete
+                                </button>
+                            </th>
+                        </tr>`
+    )
+}
+
+function updateAppointmentInList(appointment){
+
+    appointmentFields.map( field => $(`#${appointment.identifier}-${field}`).text(`${appointment[field]}`) );
+    $(`#${appointment.identifier}-btn button`).attr("onclick", `window.editAppointment(`+JSON.stringify(appointment)+`)`)
+
+}
+
+
+window.deleteAppointment = (appointment) =>{
+    const message = swal({
+            title: "Wait!",
+            text: "Are you sure to delete this appointment?",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+        }).then((willDelete) => {
+
+            if (willDelete) {
+
+                const request = $.ajax({
+                            url: `${environment.apiURL}/v1/appointments/${appointment.identifier}`,
+                            type: "delete",
+                            beforeSend: function(req) {
+                                    req.setRequestHeader("accept", "application/json");
+                                    req.setRequestHeader("Content-Type", "application/json");
+                                    req.setRequestHeader("Authorization", getCookie("Authorization"));
+                            }
+                    }); 
+                    
+            
+                    request.done((response)=>{
+                        swal("Appointment deleted", { icon: "success" });
+                        $(`#${appointment.identifier}`).remove();
+                    }); 
+            
+                    request.fail(function (jqXHR, textStatus, errorThrown){
+            
+                        swal("An error ocurred during request", { icon: "warning" });
+            
+                    });
+
+            } else {
+            }
+        });
+}
---
+++ app-assets/js/core/doctor/testimonials.js
@@ -0,0 +1,306 @@
+import environment from '../../../../environment.js'
+import { checkLogin, logout, getCookie } from '../general.js'
+
+
+$(document).ready(()=>{
+    getTestimonials();
+});
+
+const testimonialFields = ["identifier", "fecha", "name", "photo", "text_testimonials", "active"]
+let testimonialMethod = '';
+
+function getTestimonials(){
+
+    const request = $.ajax({
+        url: `${environment.apiURL}/v1/testimonials`,
+        type: "get",
+        beforeSend: function(req) {
+            req.setRequestHeader("accept", "application/json");
+            req.setRequestHeader("Content-Type", "application/json");
+            req.setRequestHeader("Authorization", getCookie("Authorization"));
+          }
+    });
+    
+    request.done(function (response, textStatus, jqXHR){
+        let testimonialsListHTML = ''
+        response.map(testimonial => {
+            testimonialsListHTML+=
+                                `<tr id='${testimonial.identifier}'>
+                                    <th id='${testimonial.identifier}-identifier'>${testimonial.identifier}</th>
+                                    <th id='${testimonial.identifier}-fecha'>${formatDate(testimonial.fecha)}</th>
+                                    <th id='${testimonial.identifier}-name'>${testimonial.name}</th>
+                                    <th id='${testimonial.identifier}-text_testimonials'>${testimonial.text_testimonials}</th>
+                                    <th id='${testimonial.identifier}-photo'>
+                                        <img style='width: 70px;' src='${environment.apiURL}/images/testimonials/${testimonial.identifier}/photo' />
+                                    </th>
+                                                                        
+                                    <th id='${testimonial.identifier}-btn' style=" display: flex; width: 180px; justify-content: space-between;">
+                                        <button onClick='window.editTestimonial(`+JSON.stringify(testimonial)+`)' type="button" class="btn btn-primary" data-toggle="modal" data-target="#reviewsModal">
+                                            Edit
+                                        </button>
+                                                                                <button onClick='window.deleteTestimonial(`+JSON.stringify(testimonial)+`)' type="button" class="btn btn-danger">
+                                            Delete
+                                        </button>
+                                    </th>
+                                </tr>`
+        });
+
+        $("#testimonials-list").html(testimonialsListHTML)
+        
+    });
+    
+    request.fail(function (jqXHR, textStatus, errorThrown){
+        alert("Se ha producido un error en la consulta de testimonials")
+    });
+
+
+
+}
+
+
+
+window.editTestimonial=(testimonial)=>{
+
+   $("#img-testimonial-edit img").show();
+
+   $("#form-error").text("")
+   testimonialFields.map(field => $(`#${field}-error`).hide() )
+
+   testimonialMethod="edit"
+   testimonialFields.map((field) => {
+       if(field!="photo") {
+        $(`#${field}`).val(String(testimonial[field]))   
+       }
+    } )
+
+    $("#fecha").val(formatDate($("#fecha").val()))
+    
+}
+
+
+window.createTestimonial=()=>{
+
+    $("#img-testimonial-edit img").hide();
+
+    $("#form-error").text("")
+    testimonialFields.map(field => $(`#${field}-error`).hide() )
+
+    testimonialMethod="create" 
+    testimonialFields.map((field) => $(`#${field}`).val(""))
+
+
+}
+
+
+window.manageTestimonial=()=>{
+
+    $("#form-error").text("")
+
+    testimonialFields.map(field =>{
+        $(`#${field}-error`).hide()
+        if(!$(`#${field}`).val()){
+            $(`#${field}-error`).show()
+        }
+    });
+
+    if(
+        !$('#name').val()     ||
+        !$("#fecha").val()   ||
+        !$("#text_testimonials").val()
+    ){
+        
+        return;
+
+    }
+
+    const identifier = $("#identifier").val();
+    const path = testimonialMethod=="create" ? "/v1/testimonials" : `/v1/testimonials/${identifier}`;
+
+    const testimonialData = {
+        name:  $('#name').val(),
+        fecha:  $("#fecha").val(),
+        active: $("#active").val(),
+        text_testimonials: $("#text_testimonials").val(),
+        app: getCookie("application"),
+    }
+
+
+
+    $("#testimonial-btn").hide()
+    $("#testimonial-spinner").show()
+    const request = $.ajax({
+        url: `${environment.apiURL}${path}`,
+        type: testimonialMethod=="create" ? "post" : "patch",
+        beforeSend: function(req) {
+            req.setRequestHeader("accept", "application/json");
+            req.setRequestHeader("Content-Type", "application/json");
+            req.setRequestHeader("Authorization", getCookie("Authorization"));
+        },
+        data: JSON.stringify(testimonialData)
+    }); 
+    
+
+     request.done((response)=>{
+
+
+        if(testimonialMethod=="create"){
+            uploadTestimonialPhoto(response, testimonialMethod);
+        } else {
+            uploadTestimonialPhoto(testimonialData, testimonialMethod);
+        }
+    }); 
+
+    request.fail(function (jqXHR, textStatus, errorThrown){
+
+        $("#testimonial-btn").show();
+        $("#testimonial-spinner").hide();
+        $("#form-error").text(jqXHR.responseJSON.message)
+        swal("Done", `An error ocurred during request`, "warning");
+
+    });
+
+
+}
+
+
+function uploadTestimonialPhoto(testimonial, testimonialMethod){
+    const iden = testimonial.identifier ? testimonial.identifier : $("#identifier").val()
+    if($('#photo')[0].files[0]){
+        const data = new FormData();
+        data.append("testimonial", $('#photo')[0].files[0])
+
+        const request = $.ajax({
+            url: `${environment.apiURL}/v1/testimonials/${iden}/upload`,
+            type: "post",
+            cache: false,
+            contentType: false,
+            processData: false,
+            beforeSend: function(req) {
+                req.setRequestHeader("Authorization", getCookie("Authorization"));
+            },
+            data: data
+        }); 
+
+
+                let actionWord=testimonialMethod=="create" ? "created" : "updated";
+
+        request.done((response)=>{
+   
+   
+           $("#testimonial-btn").show()
+           $("#testimonial-spinner").hide()
+           swal("Done", `The testimonial was ${actionWord} succefully`, "success");
+   
+   
+           if(testimonialMethod=="create"){
+               addTestimonialToList(testimonial);
+           } else {
+               updateTestimonialInLit({identifier: $('#identifier').val(), ...testimonialData});
+           }
+       }); 
+   
+       request.fail(function (jqXHR, textStatus, errorThrown){
+   
+           $("#testimonial-btn").show();
+           $("#testimonial-spinner").hide();
+           $("#form-error").text(jqXHR.responseJSON.message)
+           swal("Done", `An error ocurred during request`, "warning");
+   
+       });
+    } else {
+
+            let actionWord=testimonialMethod=="create" ? "created" : "updated";
+
+            $("#testimonial-btn").show();
+            $("#testimonial-spinner").hide();
+            swal("Done", `The testimonial was ${actionWord} succefully`, "success");
+
+            if(testimonialMethod=="create"){
+                    addTestimonialToList(testimonial);
+            } else {
+                updateTestimonialInList({identifier: $('#identifier').val(), ...testimonial});
+            }
+        }
+
+}
+
+
+
+function addTestimonialToList(testimonial){
+    $("#testimonials-list").append(
+        `<tr id='${testimonial.identifier}'>
+                                    <th id='${testimonial.identifier}-identifier'>${testimonial.identifier}</th>
+                                    <th id='${testimonial.identifier}-fecha'>${formatDate(testimonial.fecha)}</th>
+                                    <th id='${testimonial.identifier}-name'>${testimonial.name}</th>
+                                    <th id='${testimonial.identifier}-text_testimonials'>${testimonial.text_testimonials}</th>
+                                    <th id='${testimonial.identifier}-photo'>
+                                        <img style='width: 70px;' src='${environment.apiURL}/images/testimonials/${testimonial.identifier}/photo' />
+                                    </th>
+                                                                        
+                                    <th id='${testimonial.identifier}-btn' style=" display: flex; width: 180px; justify-content: space-between;">
+                                        <button onClick='window.editTestimonial(`+JSON.stringify(testimonial)+`)' type="button" class="btn btn-primary" data-toggle="modal" data-target="#reviewsModal">
+                                            Edit
+                                        </button>
+                                                                                <button onClick='window.deleteTestimonial(`+JSON.stringify(testimonial)+`)' type="button" class="btn btn-danger">
+                                            Delete
+                                        </button>
+                                    </th>
+                                </tr>`
+    )
+}
+
+function updateTestimonialInList(testimonial){
+
+        console.log(testimonial)
+    testimonialFields.map( field => $(`#${testimonial.identifier}-${field}`).text(`${testimonial[field]}`) );
+    $(`#${testimonial.identifier}-btn #edit`).attr("onclick", `window.editTestimonial(`+JSON.stringify(testimonial)+`)`)
+        $(`#${testimonial.identifier}-btn #create`).attr("onclick", `window.deleteTestimonial(`+JSON.stringify(testimonial)+`)`)
+
+}
+
+window.deleteTestimonial = (testimonial) =>{
+    const message = swal({
+            title: "Wait!",
+            text: "Are you sure to delete this testimonial?",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+        }).then((willDelete) => {
+
+            if (willDelete) {
+
+                const request = $.ajax({
+                            url: `${environment.apiURL}/v1/testimonials/${testimonial.identifier}`,
+                            type: "delete",
+                            beforeSend: function(req) {
+                                    req.setRequestHeader("accept", "application/json");
+                                    req.setRequestHeader("Content-Type", "application/json");
+                                    req.setRequestHeader("Authorization", getCookie("Authorization"));
+                            }
+                    }); 
+                    
+            
+                    request.done((response)=>{
+                        swal("Testimonial deleted", { icon: "success" });
+                        $(`#${testimonial.identifier}`).remove();
+                    }); 
+            
+                    request.fail(function (jqXHR, textStatus, errorThrown){
+            
+                        swal("An error ocurred during request", { icon: "warning" });
+            
+                    });
+
+            } else {
+            }
+        });
+}
+
+function formatDate(date){
+
+    let d = new Date(date);
+    let ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d);
+    let mo = new Intl.DateTimeFormat('en', { month: '2-digit' }).format(d);
+    let da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(d);
+    return `${ye}-${mo}-${da}`
+}
