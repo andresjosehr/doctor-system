4e28c07e9e55dd4ec979b7fb1cdce4c555d8b371
Jose Andres <joseandreshernandezross@gmail.com>
2021-05-05 13:42:02 -0400

    Changes made

--- app-assets/js/core/admin/users.js
+++ app-assets/js/core/admin/users.js
@@ -1,15 +1,15 @@
 import environment from '../../../../environment.js'
 import { checkLogin, logout, getCookie, checkLonginAdministrator } from './../general.js'
 
-$(document).ready(()=>{
+$(document).ready(() => {
     getUsers();
     getApps();
 })
 
-const userFields = ["identifier", "email", "password", "phone", "active", "type_users", "app"]
+const userFields = ["identifier", "email", "name", "password", "password2", "phone", "active", "type_users", "app"]
 let clientMethod = '';
 
-function getApps(){
+function getApps() {
 
     const request = $.ajax({
         url: `${environment.apiURL}/v1/apps`,
@@ -18,19 +18,19 @@ function getApps(){
             req.setRequestHeader("accept", "application/json");
             req.setRequestHeader("Content-Type", "application/json");
             req.setRequestHeader("Authorization", getCookie("Authorization"));
-          }
+        }
     });
 
-    request.done(function (response, textStatus, jqXHR){
+    request.done(function(response, textStatus, jqXHR) {
         let apps = ''
-        response.map(app => apps = `<option data-label="danger" value="${app.name_app}">${app.name_app}</option>` )
+        response.map(app => apps = `<option data-label="danger" value="${app.identifier}">${app.name_app}</option>`)
 
         $("#app").append(apps)
     });
 }
 
 
-function getUsers(){
+function getUsers() {
 
     const request = $.ajax({
         url: `${environment.apiURL}/v1/users`,
@@ -39,21 +39,22 @@ function getUsers(){
             req.setRequestHeader("accept", "application/json");
             req.setRequestHeader("Content-Type", "application/json");
             req.setRequestHeader("Authorization", getCookie("Authorization"));
-          }
+        }
     });
-    
-    request.done(function (response, textStatus, jqXHR){
+
+    request.done(function(response, textStatus, jqXHR) {
         let userListHTML = ''
-        response.map(user => {
-            if(getCookie("identifier")!=user.identifier){
-                userListHTML+=`<tr>
-                                    <th id='${user.identifier}-identifier'>${user.identifier}</th>
+        response.map((user, index) => {
+            if (getCookie("identifier") != user.identifier) {
+                userListHTML += `<tr>
+                                    <th style='display: none' id='${user.identifier}-identifier'>${user.identifier}</th>
+                                    <th id='${user.identifier}-counter'>${index}</th>
                                     <th id='${user.identifier}-email'>${user.email}</th>
                                     <th id='${user.identifier}-phone'>${user.phone}</th>
                                     <th id='${user.identifier}-type_users'>${user.type_users}</th>
                                     <th id='${user.identifier}-active'>${user.active}</th>
                                     <th id='${user.identifier}-btn'>
-                                        <button onClick='window.editUser(`+JSON.stringify(user)+`)' type="button" class="btn btn-primary" data-toggle="modal" data-target="#usersModal">
+                                        <button onClick='window.editUser(` + JSON.stringify(user) + `)' type="button" class="btn btn-primary" data-toggle="modal" data-target="#usersModal">
                                             Edit
                                         </button>
                                     </th>
@@ -62,10 +63,10 @@ function getUsers(){
         });
 
         $("#users-list").html(userListHTML)
-        
+
     });
-    
-    request.fail(function (jqXHR, textStatus, errorThrown){
+
+    request.fail(function(jqXHR, textStatus, errorThrown) {
         alert("Se ha producido un error en la consulta de usuarios")
     });
 
@@ -73,72 +74,94 @@ function getUsers(){
 
 }
 
-window.editUser=(user)=>{
+window.editUser = (user) => {
 
     $("#form-error").text("")
-    userFields.map(field => $(`#${field}-error`).hide() )
+    userFields.map(field => $(`#${field}-error`).hide())
 
-   clientMethod="edit"
-   userFields.map((field) => $(`#${field}`).val(String(user[field])) )
+    clientMethod = "edit"
+    userFields.map((field) => $(`#${field}`).val(String(user[field])))
 
-   $("#password").val("");
-   $("#password").prop('disabled', true);
-   $("#app").prop('disabled', true);
-   $("#type_users").prop('disabled', true);
-    
-}
-
-window.createUser=()=>{
-
-    $("#form-error").text("")
-    userFields.map(field => $(`#${field}-error`).hide() )
-
-    clientMethod="create" 
-   $("#password").prop('disabled', false);
-   $("#password").prop('disabled', false);
-   $("#app").prop('disabled', false);
-   $("#type_users").prop('disabled', false);
-   userFields.map((field) => $(`#${field}`).val(""))
+    $("#password").val("");
+    $("#password").prop('disabled', true);
+    $("#password2").val("");
+    $("#password2").prop('disabled', true);
+    $("#app").prop('disabled', true);
+    $("#type_users").prop('disabled', true);
 
 }
 
-
-window.manageUser=()=>{
+window.createUser = () => {
 
     $("#form-error").text("")
-    userFields.map(field =>{
+    userFields.map(field => $(`#${field}-error`).hide())
+
+    clientMethod = "create"
+    $("#password").prop('disabled', false);
+    $("#password").prop('disabled', false);
+    $("#app").prop('disabled', false);
+    $("#type_users").prop('disabled', false);
+    userFields.map((field) => $(`#${field}`).val(""))
+
+}
+
+
+window.manageUser = () => {
+
+
+
+    $("#form-error").text("")
+    $("#email-invalid-error").hide()
+    $(".password-match-error").hide()
+    userFields.map(field => {
         $(`#${field}-error`).hide()
-        if(!$(`#${field}`).val()){
+        if (!$(`#${field}`).val()) {
             $(`#${field}-error`).show()
         }
     });
-    if(clientMethod=="edit"){
+    if (clientMethod == "edit") {
         $("#password-error").hide()
+        $("#password2-error").hide()
     }
 
-    if(
-        !$("#email").val()       ||
-        !$("#phone").val()       ||
-        !$("#active").val()      ||
-        !$("#type_users").val()  
-    ){
-        
+    if (!$("#email").val() ||
+        !$("#phone").val() ||
+        !$("#name").val() ||
+        !$("#active").val() ||
+        !$("#type_users").val()
+    ) {
+
         return;
 
     }
 
+    if (!validateEmail($("#email").val())) {
+
+        $("#email-invalid-error").show()
+        return;
+    }
+
+    if ($("#password").val() != $("#password2").val()) {
+
+        $(".password-match-error").show()
+        return;
+    }
+
+
+
     const identifier = $("#identifier").val();
-    const path = clientMethod=="create" ? "/v1/users" : `/v1/users/${identifier}`;
+    const path = clientMethod == "create" ? "/v1/users" : `/v1/users/${identifier}`;
 
     const userData = {
-        
-            email: $("#email").val(), 
-            phone: $("#phone").val(), 
-            active: $("#active").val()=="true" ? true : false
-        
+
+        name: $("#name").val(),
+        email: $("#email").val(),
+        phone: $("#phone").val(),
+        active: $("#active").val() == "true" ? true : false
+
     };
 
-    if(clientMethod=="create"){
+    if (clientMethod == "create") {
         userData["password"] = $("#password").val();
         userData["password2"] = $("#password").val();
         userData["type_users"] = $("#type_users").val();
@@ -147,29 +170,29 @@ window.manageUser=()=>{
 
     $("#user-btn").hide()
     $("#user-spinner").show()
-    
+
     const request = $.ajax({
         url: `${environment.apiURL}${path}`,
-        type: clientMethod=="create" ? "post" : "patch",
+        type: clientMethod == "create" ? "post" : "patch",
         beforeSend: function(req) {
             req.setRequestHeader("accept", "application/json");
             req.setRequestHeader("Content-Type", "application/json");
             req.setRequestHeader("Authorization", getCookie("Authorization"));
-            
+
         },
         data: JSON.stringify(userData)
-    }); 
+    });
 
 
-    let actionWord=clientMethod=="create" ? "created" : "updated";
-     request.done((response)=>{
+    let actionWord = clientMethod == "create" ? "created" : "updated";
+    request.done((response) => {
 
         $("#user-btn").show()
         $("#user-spinner").hide()
         swal("Done", `The user was ${actionWord} succefully`, "success");
-        
-        if(clientMethod=="create"){
-            addUserToList(actionWord=clientMethod=="create" ? response : userData)
+
+        if (clientMethod == "create") {
+            addUserToList(actionWord = clientMethod == "create" ? response : userData)
         } else {
             updateUserInList({
                 identifier: $("#identifier").val(),
@@ -177,14 +200,14 @@ window.manageUser=()=>{
                 ...userData
             })
         }
-    }); 
+    });
 
-    request.fail(function (jqXHR, textStatus, errorThrown){
+    request.fail(function(jqXHR, textStatus, errorThrown) {
 
         $("#user-btn").show();
         $("#user-spinner").hide();
         $("#form-error").text(jqXHR.responseJSON.message)
-        swal("Error", `An error ocurred during request`, "warning");
+            // swal("Error", `An error ocurred during request`, "warning");
 
     });
 
@@ -192,16 +215,18 @@ window.manageUser=()=>{
 }
 
 
-function addUserToList(user){
+function addUserToList(user) {
+    const counter = parseFloat($("#DataTables_Table_0 tbody tr").last().find("th:nth-child(2)").text()) + 1
     $("#users-list").append(
         `<tr>
-                            <th id='${user.identifier}-identifier'>${user.identifier}</th>
+                            <th style='display: none' id='${user.identifier}-identifier'>${user.identifier}</th>
+                            <th id='${user.identifier}-counter'>${counter}</th>
                             <th id='${user.identifier}-email'>${user.email}</th>
                             <th id='${user.identifier}-phone'>${user.phone}</th>
                             <th id='${user.identifier}-type_users'>${user.type_users}</th>
                             <th id='${user.identifier}-active'>${user.active}</th>
                             <th id='${user.identifier}-btn'>
-                                <button onClick='window.editUser(`+JSON.stringify(user)+`)' type="button" class="btn btn-primary" data-toggle="modal" data-target="#usersModal">
+                                <button onClick='window.editUser(` + JSON.stringify(user) + `)' type="button" class="btn btn-primary" data-toggle="modal" data-target="#usersModal">
                                     Edit
                                 </button>
                             </th>
@@ -209,9 +234,23 @@ function addUserToList(user){
     )
 }
 
-function updateUserInList(user){
+function updateUserInList(user) {
 
-    userFields.map( field => $(`#${user.identifier}-${field}`).text(`${user[field]}`) );
-    $(`#${user.identifier}-btn button`).attr("onclick", `window.editUser(`+JSON.stringify(user)+`)`)
+    userFields.map(field => $(`#${user.identifier}-${field}`).text(`${user[field]}`));
+    $(`#${user.identifier}-btn button`).attr("onclick", `window.editUser(` + JSON.stringify(user) + `)`)
 
+}
+
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+window.isNumber = (evt) => {
+    evt = (evt) ? evt : window.event;
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+        return false;
+    }
+    return true;
 }
--- app-assets/js/core/login.js
+++ app-assets/js/core/login.js
@@ -3,34 +3,34 @@ import { getCookie, checkLogin } from './general.js'
 
 window.app = ''
 
-$(document).ready(()=>{
+$(document).ready(() => {
     login();
 
     const parts = window.location.href.split('/');
-    window.app = parts.pop() || parts.pop();  // handle potential trailing slash
+    window.app = parts.pop() || parts.pop(); // handle potential trailing slash
 
 });
 
-function checkLonginL(){
+function checkLonginL() {
 
     const request = checkLogin();
 
-    request.done(function (response, textStatus, jqXHR){
+    request.done(function(response, textStatus, jqXHR) {
 
         console.log($(location).attr('href'), environment.appUrl)
 
-            if(response.type_users=="admins"){
-                window.location.href = "./admin/calendar.html";
-            }
+        if (response.type_users == "admins") {
+            window.location.href = "./admin/calendar.html";
+        }
 
-            if(response.type_users=="doctors"){
-                window.location.href = "./doctor/calendar.html";
-            }
-            $("#login-btn").show()
-            $("#login-spinner").hide()
+        if (response.type_users == "doctors") {
+            window.location.href = "./doctor/calendar.html";
+        }
+        $("#login-btn").show()
+        $("#login-spinner").hide()
     });
 
-    request.fail(function (jqXHR, textStatus, errorThrown){
+    request.fail(function(jqXHR, textStatus, errorThrown) {
         $("#login-btn").show()
         $("#login-spinner").hide()
     });
@@ -38,11 +38,11 @@ function checkLonginL(){
 
 }
 
-function login(){
+function login() {
 
-    $("#login-form").submit(function(e){
-        
-        if($("#login-form").valid()){
+    $("#login-form").submit(function(e) {
+
+        if ($("#login-form").valid()) {
 
             e.preventDefault();
 
@@ -61,20 +61,20 @@ function login(){
                 beforeSend: function(req) {
                     req.setRequestHeader("accept", "application/json");
                     req.setRequestHeader("Content-Type", "application/json");
-                  },
-                data: JSON.stringify({ email, password, app: application })
+                },
+                data: JSON.stringify({ email, password, app: application, auth_type: "web" })
             });
-            
-            request.done(function (response, textStatus, jqXHR){
+
+            request.done(function(response, textStatus, jqXHR) {
 
                 document.cookie = `identifier=${response.identifier}; Path=/;`;
                 document.cookie = `Authorization=${response.token}; Path=/;`;
                 document.cookie = `application=${application}; Path=/;`;
                 checkLonginL();
-                
+
             });
-            
-            request.fail(function (jqXHR, textStatus, errorThrown){
+
+            request.fail(function(jqXHR, textStatus, errorThrown) {
 
                 $("#request-error").text(jqXHR.responseJSON.message)
                 $("#request-error").show()
