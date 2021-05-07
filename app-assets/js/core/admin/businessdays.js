7d96147eff2e952b9c4b0bc0e0c8c590e159dc81
Jose Andres Hernandez <joseandreshernandezross@gmail.com>
2021-05-07 13:20:23 -0400

    Bussines days fixed

--- app-assets/js/core/admin/businessdays.js
+++ app-assets/js/core/admin/businessdays.js
@@ -4,11 +4,16 @@ import { checkLogin, logout, getCookie, checkLonginAdministrator } from './../ge
 
 $(document).ready(()=>{
     getApp();
+    getbranches();
+    getUsers();
 })
 
 const dayOfTheWeek= [ "sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday" ]
 let apps;
 let appSelected;
+let branches;
+let users;
+let userSelected;
 
 function getApp(){
 
@@ -36,28 +41,7 @@ function getApp(){
         $(`#apps`).val(appSelected.identifier)
         $(`#apps option[value="${appSelected.identifier}"]`).prop('selected', true);
 
-        Object.entries(appSelected.work_weeks).map((day) =>{
-
-            $(`#${day[0]} .start`).val(day[1].start)
-            $(`#${day[0]} .end`).val(day[1].end)
-            $(`#${day[0]} .active`).val(day[1].active)
-            $(`#${day[0]} .active option[value="${day[1].active}"]`).prop('selected', true);
-
-            flatpickr(`#${day[0]} .start`, {
-                noCalendar: true,
-                enableTime: true,
-                dateFormat: "H:i",
-                defaultDate: new Date("2021-04-06 "+day[1].start)
-            });
-
-            flatpickr(`#${day[0]} .end`, {
-                noCalendar: true,
-                enableTime: true,
-                dateFormat: "H:i",
-                defaultDate: new Date("2021-04-06 "+day[1].end)
-            });
-
-        })
+        window.changeBranches();
         
     });
     
@@ -69,12 +53,50 @@ function getApp(){
 
 }
 
+function getbranches() {
+
+    const request = $.ajax({
+        url: `${environment.apiURL}/v1/branches`,
+        type: "get",
+        beforeSend: function(req) {
+            req.setRequestHeader("accept", "application/json");
+            req.setRequestHeader("Content-Type", "application/json");
+            req.setRequestHeader("Authorization", getCookie("Authorization"));
+        }
+    });
+
+    request.done(function(response, textStatus, jqXHR) {
+        branches = response
+    });
+}
+
+
+function getUsers() {
+
+    const request = $.ajax({
+        url: `${environment.apiURL}/v1/users`,
+        type: "get",
+        beforeSend: function(req) {
+            req.setRequestHeader("accept", "application/json");
+            req.setRequestHeader("Content-Type", "application/json");
+            req.setRequestHeader("Authorization", getCookie("Authorization"));
+        }
+    });
+
+    request.done(function(response, textStatus, jqXHR) {
+        console.log(response)
+        users = response
+    });
+}
+
+
+
 window.updateWorkWeek = (day) =>{
 
 
 
     const request = $.ajax({
-        url: `${environment.apiURL}/v1/apps/${appSelected.identifier}/week/${capitalizeFirstLetter(day)}`,
+        url: `${environment.apiURL}/v1/users/${userSelected.identifier}/week/${capitalizeFirstLetter(day)}`,
         type: "patch",
         beforeSend: function(req) {
             req.setRequestHeader("accept", "application/json");
@@ -92,7 +114,7 @@ window.updateWorkWeek = (day) =>{
 
     request.done((response)=>{
 
-        apps.map(app =>{
+/*         apps.map(app =>{
             if(app.identifier==appSelected.identifier){
                 app.work_weeks[day] = {
                     start:  $(`#${day} .start`).val(),
@@ -100,7 +122,7 @@ window.updateWorkWeek = (day) =>{
                     active: $(`#${day} .active`).val()=="true"
                   }
             }
-        })
+        }) */
 
         swal("Done", `The day has been updated succefully`, "success");
     }); 
@@ -118,17 +140,99 @@ function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
-  window.changeApp=()=>{
-
-      appSelected = apps.find(app => app.identifier==$("#apps").val())
 
 
-      Object.entries(appSelected.work_weeks).map((day) =>{
+  window.changeBussinesDays=()=>{
 
-        $(`#${day[0]} .start`).val(day[1].start)
-        $(`#${day[0]} .end`).val(day[1].end)
-        $(`#${day[0]} .active`).val(day[1].active)
-        $(`#${day[0]} .active option[value="${day[1].active}"]`).prop('selected', true);
-    });
+        $(".start").prop('disabled', false);
+        $(".end").prop('disabled', false);
+        $(".active").prop('disabled', false);
+        $(".save").prop('disabled', false);
+    
+    if($("#users").val()==null){
+        $(".start").prop('disabled', true);
+        $(".end").prop('disabled', true);
+        $(".active").prop('disabled', true);
+        $(".save").prop('disabled', true);
+
+        $(".start").val('');
+        $(".end").val('');
+
+        return false;
+    }
+
+    const request = $.ajax({
+            url: `${environment.apiURL}/v1/users/${$("#users").val()}`,
+            type: "get",
+            beforeSend: function(req) {
+                req.setRequestHeader("accept", "application/json");
+                req.setRequestHeader("Content-Type", "application/json");
+                req.setRequestHeader("Authorization", getCookie("Authorization"));
+            }
+        });
+
+        request.done(function(response, textStatus, jqXHR) {
+            userSelected = response
+            console.log(userSelected)
+
+            Object.entries(userSelected.work_weeks).map((day) =>{
+
+                /* $(`#${day[0]} .start`).val(day[1].start)
+                $(`#${day[0]} .end`).val(day[1].end) */
+
+                flatpickr(`#${day[0]} .start`, {
+                    noCalendar: true,
+                    enableTime: true,
+                    dateFormat: "H:i",
+                    defaultDate: new Date("2021-04-06 "+day[1].start)
+                });
+
+                flatpickr(`#${day[0]} .end`, {
+                    noCalendar: true,
+                    enableTime: true,
+                    dateFormat: "H:i",
+                    defaultDate: new Date("2021-04-06 "+day[1].end)
+                });
+                
+
+                $(`#${day[0]} .active`).val(day[1].active)
+                $(`#${day[0]} .active option[value="${day[1].active}"]`).prop('selected', true);
+
+
+                    
+            });
+
+        });
+
+      /* userSelected = users.find(user => user.identifier==$("#users").val()) */
+
+
 
   }
+
+
+  window.changeBranches = () => {
+
+    if(!branches){
+        return window.changeUsers();
+    }
+    
+    let branchesOptions = ''
+    $("#branches").html("")
+    branches.map(branch => branchesOptions += branch.app==$("#apps").val() ? `<option data-label="danger" value="${branch.identifier}">${branch.name}</option>` : "")
+    $("#branches").append(branchesOptions)
+    window.changeUsers();
+}
+
+window.changeUsers = () => {
+
+    if(!users){
+        return window.changeBussinesDays();
+    }
+
+    let userOptions = ''
+    $("#users").html("")
+    users.map(user => userOptions += user.branch==$("#branches").val() ? `<option data-label="danger" value="${user.identifier}">${user.name}</option>` : "")
+    $("#users").append(userOptions)
+    window.changeBussinesDays();
+}
